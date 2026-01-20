6c2220ae9c3b69391fa265c6bfe7fee691ca65f9
MSRCAM83 <143851120+MSRCAM83@users.noreply.github.com>
2026-01-19 19:28:04 -0600

    v2.12: Fix portal form visibility, embed rate card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Menu, X, Sun, Moon } from 'lucide-react';
+// eslint-disable-next-line no-unused-vars
 import { colors, LYT_INFO } from './config/constants';
 
 // Pages
