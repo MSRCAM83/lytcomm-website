71eafe5111384ca7236ab6162812b7e9e6600ed9
MSRCAM83 <143851120+MSRCAM83@users.noreply.github.com>
2026-01-23 22:58:40 -0600

    Revert to working v4.2 Apps Script URL

--- src/config/constants.js
+++ src/config/constants.js
@@ -28,7 +28,7 @@ export const LYT_INFO = {
 
 export const URLS = {
   // UPDATED Jan 23, 2026 - Apps Script v4.3 (JSON parsing fix)
-  appsScript: 'https://script.google.com/macros/s/AKfycbxbzBc2ll66zJ5lXA3WYu5qgGXwalPww6VRiPrG4FRKjtRhGuDPG6kK_LrgAmS8naMI/exec',
+  appsScript: 'https://script.google.com/macros/s/AKfycbyjQrJu0BbREwo-LW2ajXKrrjQ_HrtU_IYdhz8SqDo0f4HOktHShaIHY_17pYTkN5Bm/exec',
   googleDriveFolder: '11EuU2K-DzaT9KrDdbKOI4Q21c0-jKtiC',
   rateCardSheet: '10Py5x0vIUWPzKn1ZeTaIGyaEJonbz-0BHmSYV-20rB4',
   w4Pdf: '/Form%20W-4%20sign.pdf',
