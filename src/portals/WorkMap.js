54356745fbb1253066de2ca5793188762179d7df
MSRCAM83 <143851120+MSRCAM83@users.noreply.github.com>
2026-01-29 01:20:39 -0600

    Gateway v2.0 URL

--- src/portals/WorkMap.js
+++ src/portals/WorkMap.js
@@ -8,7 +8,7 @@ import {
 } from 'lucide-react';
 import { colors } from '../config/constants';
 
-const GATEWAY_URL = 'https://script.google.com/macros/s/AKfycbyFWHLgFOglJ75Y6AGnyme0P00OjFgE_-qrDN9m0spn4HCgcyBpjvMopsB1_l9MDjIctQ/exec';
+const GATEWAY_URL = 'https://script.google.com/macros/s/AKfycbxKqHqIRbYHyjN5sCz7iBJs6WCg1Xt5BdTsMQ3YLspe15UWJ_Qsf_IOa9qapcln_JDS/exec';
 const GATEWAY_SECRET = 'LYTcomm2026ClaudeGatewaySecretKey99';
 const SHEET_ID = '1VciM5TqHC5neB7JzpcFkX0qyoyzjBvIS0fKkOXQqnrc';
 
