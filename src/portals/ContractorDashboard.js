9446daf48557b737b009534e776e8e4469dc1f57
MSRCAM83 <143851120+MSRCAM83@users.noreply.github.com>
2026-01-30 11:28:19 -0600

    v2.2.1: ContractorDashboard - Fixed CORS (text/plain content-type for mobile)

--- src/portals/ContractorDashboard.js
+++ src/portals/ContractorDashboard.js
@@ -35,7 +35,7 @@ const submitToSheet = async (sheetName, rowData) => {
   try {
     const text = await fetchWithRedirect(GATEWAY_URL, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'text/plain;charset=utf-8' },
       body: JSON.stringify({
         secret: GATEWAY_SECRET,
         action: 'sheetsAppend',
@@ -232,7 +232,7 @@ const ContractorDashboard = ({ setCurrentPage, loggedInUser, setLoggedInUser, da
       try {
         const text = await fetchWithRedirect(GATEWAY_URL, {
           method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
+          headers: { 'Content-Type': 'text/plain;charset=utf-8' },
           body: JSON.stringify({
             secret: GATEWAY_SECRET,
             action: 'sheetsRead',
